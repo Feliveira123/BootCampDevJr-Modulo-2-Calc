8a1b549cc7cdc071042cfd8d776cda86dc83c407
Feliveira123 <felipeferreiradeoliveira438@gmail.com>
2024-03-15 22:46:03 -0300

    Closes 2, Closes #3 - CSS Externo e botão somar

---
+++ index.js
@@ -0,0 +1,8 @@
+function calc(){
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+
+    let output = num1 + num2;
+
+    document.getElementById("output").value = output;
+}
