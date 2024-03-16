c1f1cbb8efb9e62eef0612767cd1ce14a435f39c
Feliveira123 <felipeferreiradeoliveira438@gmail.com>
2024-03-15 22:57:53 -0300

    Closes #4 - 4 operações lógicas

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
-function calc(){
+function sum(){
     let num1 = parseFloat(document.getElementById("num1").value);
     let num2 = parseFloat(document.getElementById("num2").value);
 
     let output = num1 + num2;
+
+    document.getElementById("output").value = output;
+}
+
+function sub(){
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+
+    let output = num1 - num2;
+
+    document.getElementById("output").value = output;
+}
+
+function mult(){
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+
+    let output = num1 * num2;
+
+    document.getElementById("output").value = output;
+}
+
+function div(){
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+
+    let output = num1 / num2;
 
     document.getElementById("output").value = output;
 }
